8975a160f5d2daa017a8454391f0d0a4359bdbb7
Miguel <msevillagarrido@safareyes.es>
2025-01-24 12:48:49 +0100

    He perfilado las pantallas que diseñe para que se adecuen exactamente al Figma

--- src/app/mensajeria/mensajeria.component.ts
+++ src/app/mensajeria/mensajeria.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {AnimationController, IonicModule} from "@ionic/angular";
 import {CommonModule} from "@angular/common";
-import {arrowBackCircle, personCircle, chatbubblesOutline, micOutline, happyOutline, cameraOutline} from "ionicons/icons";
+import {arrowBackCircle, personOutline, chatbubblesOutline, micOutline, happyOutline, cameraOutline} from "ionicons/icons";
 import {addIcons} from "ionicons";
 
 @Component({
@@ -18,7 +18,7 @@ export class MensajeriaComponent {
 
   constructor(private animationCtrl: AnimationController) {
 
-    addIcons( { arrowBackCircle, personCircle, chatbubblesOutline, micOutline, happyOutline, cameraOutline } );
+    addIcons( { arrowBackCircle, personOutline, chatbubblesOutline, micOutline, happyOutline, cameraOutline } );
   }
 
   entrarAnimacion = (baseEl: HTMLElement) => {
@@ -41,7 +41,7 @@ export class MensajeriaComponent {
       .create()
       .addElement(baseEl)
       .easing('ease-out')
-      .duration(500)
+      .duration(100)
       .addAnimation([backdropAnimation, wrapperAnimation]);
   };
 
