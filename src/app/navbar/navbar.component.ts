23fa511b08ebe22f0f28ffb144378cf518906642
Jesus <jmorenojimenez@safareyes.es>
2025-01-27 09:55:22 +0100

    detalles publicaciones finalizado

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import {IonicModule} from "@ionic/angular";
-import {addIcons} from "ionicons";
-import {menuOutline, searchOutline} from "ionicons/icons";
+import { IonicModule } from '@ionic/angular';
+import { addIcons } from 'ionicons';
+import { menuOutline, searchOutline } from 'ionicons/icons';
+import { CommonModule } from '@angular/common';
+import {FormsModule} from "@angular/forms"; // Importar CommonModule
 
 @Component({
   selector: 'app-navbar',
@@ -9,15 +11,26 @@ import {menuOutline, searchOutline} from "ionicons/icons";
   styleUrls: ['./navbar.component.scss'],
   standalone: true,
   imports: [
-    IonicModule
+    IonicModule,
+    CommonModule,
+    FormsModule,
+    // Asegúrate de agregar CommonModule aquí
   ]
 })
-export class NavbarComponent  implements OnInit {
+export class NavbarComponent implements OnInit {
+
+  isSearchVisible: boolean = false;  // Variable que controla la visibilidad del input
+  searchQuery: string = '';          // Variable para almacenar el texto ingresado en el input
 
   constructor() {
-    addIcons({menuOutline,searchOutline})
+    addIcons({ menuOutline, searchOutline });
   }
 
   ngOnInit() {}
+
+  // Método para alternar la visibilidad del input
+  toggleSearchInput() {
+    this.isSearchVisible = !this.isSearchVisible;
+  }
 
 }
