f8aeb99d02be6c92624f474814a7600d5c564539
Miguel <msevillagarrido@safareyes.es>
2025-02-18 14:23:24 +0100

    Termine casi el responsive para administracion, validacion completada con toast

--- src/app/administracion/administracion.component.ts
+++ src/app/administracion/administracion.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { IonicModule, ModalController } from "@ionic/angular";
-import { settingsOutline, banOutline, personCircleOutline, arrowBackCircle, caretUpOutline } from "ionicons/icons";
+import { settingsOutline, banOutline, personCircleOutline, arrowBackCircle, caretUpOutline, arrowForwardOutline } from "ionicons/icons";
 import { addIcons } from "ionicons";
 import { CommonModule } from "@angular/common";
 import { FormsModule } from "@angular/forms";
@@ -11,6 +11,9 @@ import {Publicacion} from "../modelos/Publicacion";
 import {PublicacionService} from "../services/publicacion.service";
 import {Router} from "@angular/router";
 import {LoginService} from "../services/login.service";
+import {CabeceraComponent} from "../cabecera/cabecera.component";
+import { ToastService} from "../services/toast.service";
+import {CabeceraSinRutaComponent} from "../cabecera-sin-ruta/cabecera-sin-ruta.component";
 
 @Component({
   selector: 'app-administracion',
@@ -20,7 +23,9 @@ import {LoginService} from "../services/login.service";
   imports: [
     IonicModule,
     CommonModule,
-    FormsModule
+    FormsModule,
+    CabeceraSinRutaComponent,
+    CabeceraComponent
   ]
 })
 export class AdministracionComponent implements OnInit {
@@ -34,8 +39,8 @@ export class AdministracionComponent implements OnInit {
 
   vistaActual: 'admin' | 'users' | 'baneados' = 'admin';
 
-  constructor(private perfilService: PerfilService, private router : Router, private loginService : LoginService) {
-    addIcons({ settingsOutline, banOutline, personCircleOutline, arrowBackCircle, caretUpOutline });
+  constructor(private toast: ToastService , private perfilService: PerfilService, private router : Router, private loginService : LoginService) {
+    addIcons({ settingsOutline, banOutline, personCircleOutline, arrowBackCircle, caretUpOutline, arrowForwardOutline });
   }
 
   ngOnInit() { }
@@ -45,9 +50,17 @@ export class AdministracionComponent implements OnInit {
     this.perfilService.getPerfilesBaneados().subscribe({
       next: (data) => {
         this.perfiles = data;
+        if (this.perfiles.length === 0) {
+          this.toast.presentToast('No hay usuarios baneados.', 'warning');
+        } else {
+          this.vistaActual = 'baneados';
+        }
         console.info(data);
       },
-      error: (error) => console.error('Error:', error),
+      error: (error) => {
+        console.error('Error:', error);
+        this.toast.presentToast('Error al cargar usuarios baneados.', 'error');
+      },
       complete: () => {
         console.log('Petición completada');
       },
@@ -61,7 +74,10 @@ export class AdministracionComponent implements OnInit {
         this.perfiles = data;
         console.info(data);
       },
-      error: (error) => console.error('Error:', error),
+      error: (error) => {
+        console.error('Error:', error);
+        this.toast.presentToast('Error al cargar usuarios no baneados.', 'error');
+      },
       complete: () => {
         console.log('Petición completada');
       },
@@ -71,10 +87,14 @@ export class AdministracionComponent implements OnInit {
   viajarA(view: 'admin' | 'users' | 'baneados') {
     this.vistaActual = view;
 
+    if (view === 'admin') {
+      return;
+    }
+
     if (view === 'users') {
-      this.cargarNoBaneados();  // Cargar usuarios no baneados
+      this.cargarNoBaneados();
     } else if (view === 'baneados') {
-      this.cargarBaneados();  // Cargar usuarios baneados
+      this.cargarBaneados();
     }
   }
 
@@ -104,10 +124,32 @@ export class AdministracionComponent implements OnInit {
     this.perfilService.banearPerfil(correo).subscribe({
       next: (data) => {
         console.log("Usuario baneado correctamente");
+        this.toast.presentToast('Usuario baneado correctamente.', 'success');
+        this.cargarNoBaneados();
       },
-      error: (error) => console.error('Error:', error),
+      error: (error) => {
+        console.error('Error:', error);
+        this.toast.presentToast('Error al banear usuario.', 'error');
+      },
       complete: () => {
         console.log('Usuario baneado exitosamente');
+      },
+    });
+  }
+
+  desbanearUsuario(correo: string | undefined) {
+    this.perfilService.desbanearPerfil(correo).subscribe({
+      next: () => {
+        console.log("Usuario desbaneado correctamente");
+        this.toast.presentToast('Usuario desbaneado correctamente.', 'success');
+        this.cargarBaneados();
+      },
+      error: (error) => {
+        console.error('Error:', error);
+        this.toast.presentToast('Error al desbanear usuario.', 'error');
+      },
+      complete: () => {
+        console.log('Usuario desbaneado exitosamente');
       },
     });
   }
