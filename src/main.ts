1d143d06a17a3c2bec8b2586e6dae0420f8060c3
jesus <jmorenojimenez@safareyes.es>
2025-02-20 01:39:44 +0100

    intento de optimizacion 8

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,17 @@
-import {bootstrapApplication} from '@angular/platform-browser';
-import {
-  PreloadAllModules,
-  provideRouter,
-  RouteReuseStrategy,
-  withComponentInputBinding,
-  withPreloading
-} from '@angular/router';
-import {IonicRouteStrategy, provideIonicAngular} from '@ionic/angular/standalone';
-
-import {routes} from './app/app.routes';
-import {AppComponent} from './app/app.component';
-import {provideHttpClient} from "@angular/common/http";
-import {LocationStrategy, PathLocationStrategy} from "@angular/common";
+// src/main.ts
+import './polyfills';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
+import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
+import { routes } from './app/app.routes';
+import { AppComponent } from './app/app.component';
+import { provideHttpClient } from '@angular/common/http';
 
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-    { provide: LocationStrategy, useClass: PathLocationStrategy },  // 🔥 FORZA RUTAS SIN #
-    provideRouter(routes, withComponentInputBinding(), withPreloading(PreloadAllModules)),
     provideIonicAngular(),
-    provideHttpClient(),
+    provideRouter(routes, withPreloading(PreloadAllModules)),
+    provideHttpClient()
   ],
 });
